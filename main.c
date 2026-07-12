982a4e5bf1ff8bf7d26fa12ea47986d34757cfd0
Furgan Gurbanli <f.qurbanli.2006@gmail.com>
2026-07-12 12:31:05 +0400

    ProcessMatrix

--- main.c
+++ main.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
 int GetIntInput();
-int ProcessMatrix(int row, int column);
+void ProcessMatrix(int row, int column);
+
 
 int main(void)
 {
+    printf("Enter a valid value for row count: ");
+    int row = GetIntInput();
+    printf("Enter a valid value for column count: ");
+    int column = GetIntInput();
+    ProcessMatrix(row, column);
 
-
+    return 0;
 }
 
 int GetIntInput()
@@ -19,7 +25,30 @@ int GetIntInput()
             break;
         }
         printf("Enter a valid value!\n");
+        while (getchar() != '\n');
     }
     return input;
 }
 
+void ProcessMatrix(int row, int column)
+{
+    int matrix [row] [column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            printf("Give value for [%d] [%d]: ", i, j );
+            matrix [i] [j] = GetIntInput();
+        }
+    }
+    printf("Here is your matrix!\n");
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            printf("%d ", matrix [i] [j]);
+        }
+        printf("\n");
+    }
+
+}
